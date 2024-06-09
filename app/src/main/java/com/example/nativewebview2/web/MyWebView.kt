bc4d2d77032781ea1961b94c2ff059b8340cde30
Bruno Lima <brunojet@gmail.com>
2024-06-09 07:11:50 -0300

    Primeira versao que funciona

---
+++ app/src/main/java/com/example/nativewebview2/web/MyWebView.kt
@@ -0,0 +1,128 @@
+package com.example.nativewebview2.web
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.ViewGroup
+import android.view.WindowInsets
+import android.webkit.WebSettings
+import android.webkit.WebView
+import com.example.nativewebview2.R
+
+class MyWebView(
+    private val context: Context,
+    private val webView: WebView,
+    private val baseUrl: String,
+    private val webViewCallback: WebViewCallback
+) {
+    interface WebViewCallback {
+        fun onWebViewLoaded()
+        fun onLoadError()
+        fun onNetworkError()
+    }
+
+    private val webViewStartTimeout = 30000L
+
+    private var handler: Handler? = null
+
+    private var watchdogTriggered = Runnable {
+        Log.d(this.javaClass.simpleName, "Watchdog triggered - Stopping loading and handling error")
+        webView.stopLoading()
+        webViewCallback.onLoadError()
+    }
+
+    init {
+        WebView.setWebContentsDebuggingEnabled(false)
+        startWebView()
+        startWatchdog()
+    }
+
+    private fun startWatchdog() {
+        stopWatchdog()
+        Log.d(
+            this.javaClass.simpleName,
+            "Starting watchdog with timeout: $webViewStartTimeout milliseconds"
+        )
+        handler = Handler(Looper.getMainLooper())
+        handler?.postDelayed(watchdogTriggered, webViewStartTimeout)
+    }
+
+    private fun stopWatchdog() {
+        if (handler != null) {
+            Log.d(this.javaClass.simpleName, "Stopping watchdog")
+            handler?.removeCallbacks(watchdogTriggered)
+            handler = null
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun startWebView() {
+        Log.d(
+            this.javaClass.simpleName, "<startWebView url: $baseUrl>"
+        )
+        val webViewClient = MyWebViewClient()
+        val webAppInterface = MyWebAppInterface()
+        webViewClient.setWebViewCallback(MyWebViewClientCallback())
+        webAppInterface.setWebAppInterfaceCallback(MyWebAppInterfaceCallback())
+        webView.addJavascriptInterface(
+            webAppInterface, context.getString(R.string.view_javascript_interface)
+        )
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+        webView.webViewClient = webViewClient
+        webView.setOnApplyWindowInsetsListener { v, insets ->
+            val bottomInset = when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> insets.getInsets(WindowInsets.Type.systemBars()).bottom
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
+                    @Suppress("DEPRECATION") insets.systemGestureInsets.bottom
+                }
+
+                else -> {
+                    @Suppress("DEPRECATION") insets.systemWindowInsetBottom
+                }
+            }
+
+            v.setPadding(
+                v.paddingLeft, v.paddingTop, v.paddingRight, bottomInset
+            )
+
+            insets
+        }
+
+        webView.loadUrl(baseUrl)
+
+        Log.d(this.javaClass.simpleName, "</startWebView>")
+    }
+
+    fun stopWebView() {
+        Log.d(this.javaClass.simpleName, "<stopWebView $webView.visibility>")
+        val parent = webView.parent as ViewGroup?
+        parent?.removeView(webView)
+        webView.removeJavascriptInterface(context.getString(R.string.view_javascript_interface))
+        webView.destroy()
+        Log.d(this.javaClass.simpleName, "</stopWebView>")
+    }
+
+    inner class MyWebViewClientCallback : MyWebViewClient.WebViewClientCallback {
+        override fun onPageStarted() {
+            stopWatchdog()
+        }
+
+        override fun onPageFinished(url: String) {}
+
+        override fun onReceivedError() {
+            stopWatchdog()
+            webViewCallback.onNetworkError()
+        }
+    }
+
+    inner class MyWebAppInterfaceCallback : MyWebAppInterface.WebAppInterfaceCallback {
+        override fun finishLoading() {
+            webViewCallback.onWebViewLoaded()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/nativewebview2/web/MyWebViewClient.kt
@@ -0,0 +1,77 @@
+package com.example.nativewebview2.web
+
+import android.graphics.Bitmap
+import android.util.Log
+import android.webkit.RenderProcessGoneDetail
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MyWebViewClient : WebViewClient() {
+    interface WebViewClientCallback {
+        fun onPageStarted()
+        fun onPageFinished(url: String)
+        fun onReceivedError()
+    }
+
+    private var webViewCallback: WebViewClientCallback? = null
+
+    fun setWebViewCallback(callback: WebViewClientCallback?) {
+        webViewCallback = callback
+    }
+
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        super.onPageStarted(view, url, favicon)
+        Log.d(this.javaClass.simpleName, "onPageStarted: $url")
+        webViewCallback?.onPageStarted()
+    }
+
+    override fun onReceivedHttpError(
+        view: WebView?, request: WebResourceRequest?, errorResponse: WebResourceResponse?
+    ) {
+        super.onReceivedHttpError(view, request, errorResponse)
+        Log.d(
+            this.javaClass.simpleName,
+            "onReceivedHttpError: ${request?.url} Error: ${errorResponse?.reasonPhrase}"
+        )
+    }
+
+    override fun onScaleChanged(view: WebView?, oldScale: Float, newScale: Float) {
+        super.onScaleChanged(view, oldScale, newScale)
+        Log.d(this.javaClass.simpleName, "onScaleChanged: $newScale")
+    }
+
+    override fun onRenderProcessGone(view: WebView?, detail: RenderProcessGoneDetail?): Boolean {
+        Log.d(this.javaClass.simpleName, "onRenderProcessGone: ${detail ?: ""} ")
+        return super.onRenderProcessGone(view, detail)
+    }
+
+    override fun onPageFinished(view: WebView?, url: String?) {
+        super.onPageFinished(view, url)
+        Log.d(this.javaClass.simpleName, "onPageFinished: $url")
+        webViewCallback?.onPageFinished(url ?: "")
+    }
+
+    override fun onReceivedError(
+        view: WebView?, request: WebResourceRequest?, error: WebResourceError?
+    ) {
+        super.onReceivedError(view, request, error)
+        Log.d(
+            this.javaClass.simpleName,
+            "onReceivedError: ${request?.url} Error: ${error?.description}/${error}"
+        )
+        val errorCode = error?.errorCode ?: -1
+
+        when (errorCode) {
+            ERROR_HOST_LOOKUP -> {
+                webViewCallback?.onReceivedError()
+            }
+            else -> {
+                Log.e(this.javaClass.simpleName, "onReceivedError: ${error?.description}($errorCode)")
+            }
+        }
+
+    }
+}
